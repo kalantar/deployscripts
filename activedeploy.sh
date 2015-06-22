066f959c6e56ed387917a6a9ef071f5220b307e0
Michael Kalantar <kalantar@us.ibm.com>
2015-06-22 23:23:05 +0000

    what is already there

--- activedeploy.sh
+++ activedeploy.sh
@@ -7,25 +7,6 @@ set -x
 install_cf() {
   local __target_loc=${1}
   
-  if [[ -z $(which cf) ]]; then
-    local __tmp=/tmp/cf$$.tgz
-    wget -O ${__tmp} 'https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.11.3&source=github-rel'
-    tar -C ${__target_loc} -xzf ${__tmp}
-    rm -f ${__tmp}
-  fi
-}
-
-install_active_deploy() {
-  if [[ -z $(cf list-plugin-repos | grep "bluemix-staging") ]]; then
-    cf add-plugin-repo bluemix-staging http://plugins.stage1.ng.bluemix.net
-  fi
-  cf uninstall-plugin active-deploy || true
-  cf install-plugin active-deploy -r bluemix-staging
-}
-
-install_cf() {
-  local __target_loc=${1}
-  
 #  if [[ -z $(which cf) ]]; then
     local __tmp=/tmp/cf$$.tgz
     wget -O ${__tmp} 'https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.11.3&source=github-rel'
@@ -59,6 +40,10 @@ install_active_deploy
 cf plugins
 
 cf active-deploy-list
+
+ice group list
+ice ps
+ice images
 
 ## Identify any existing group
 #TRUNC_PREFIX=$(echo ${PREFIX} | cut -c 1-16)
